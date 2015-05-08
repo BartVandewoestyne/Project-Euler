5254a16262ad647d2734df14568d14da0f261f6b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2015-05-08 13:23:39 +0200

    Get problem 3 up and running.

--- Python/Problem003.py
+++ Python/Problem003.py
@@ -1,6 +1,22 @@
-import numpy as np
+# TODO:
+#   * Check if this is correct for the number 600851475143
+#   * Don't reinvent the wheel!  Remove the user-defined prime_factors function and use a library!
 
-a = np.array([0, 1, 2, 3])
 
-print str(a)
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
+number = 600851475143
+prime_factors = prime_factors(number)
+print 'Prime factors of ' + str(number) + ': ' + str(prime_factors)
+print 'Max of the prime factors = ' + str(max(prime_factors))
