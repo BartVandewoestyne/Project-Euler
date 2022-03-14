e3a515afd7f5c2f311dc7d33a23028dff67b240e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-03-14 16:01:31 +0100

    Add C++ solution for Problem 3.

---
+++ C++/problem003.cpp
@@ -0,0 +1,31 @@
+/*
+ * References:
+ *   https://www.tutorialspoint.com/prime-factor-in-cplusplus-program
+ */
+
+#include <iostream>
+#include <cmath>
+
+long int getLargestPrimeFactor(long int n)
+{
+    while (n % 2 == 0)
+    {
+       n = n / 2;
+    }
+
+   for (long int i = 3; i <= std::sqrt(n); i += 2)
+   {
+      while (n % i == 0)
+      {
+         n = n / i;
+      }
+   }
+   return n;
+}
+
+int main() {
+   const auto n = 600851475143;
+   const auto largestPrimeFactor = getLargestPrimeFactor(n);
+   std::cout << largestPrimeFactor << std::endl;
+   return 0;
+}
